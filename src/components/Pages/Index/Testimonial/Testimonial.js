cbe85414f403a3bf6dd3beb96723c5609efbdd98
Tarek Ahmed <tarek5701@gmail.com>
2021-12-19 12:23:57 -0800

    Added config files to hold all the values

--- src/components/Pages/Index/Testimonial/Testimonial.js
+++ src/components/Pages/Index/Testimonial/Testimonial.js
@@ -1,30 +1,14 @@
 import React from "react"
 import { Carousel } from "react-bootstrap"
 import * as style from "./Testimonial.module.css"
+import config from "../../../../../config"
 
-const contents = [
-    {
-        title: "Zahid Un-Nabi",
-        message:
-            "Tarek is one of the best developers I've worked with. I was his team leader in The Cookups Technology. He is a fast learner and a detail-oriented person. Besides, that is his thought process and punctuality is truly appreciatable. If you are seeking a dynamic-minded developer Tarek is the one. Wishing you the best. Always keep faith in yourself.",
-    },
-    {
-        title: "Toufiqur Rahman",
-        message:
-            "I love to say lot of appreciations about tarek. but the first thing I must say is, this guy is horribly hard working. He's a good knowledge in programming as well as a good app developer. Recently his dedication about python and django impressed me heavily. there are lot of positiveness about him like leadership ability, team player etc. I know him for last 6 years. as a friend he's 8/10 and as human being 9/10...may be it should be 10/10 but in my perception no one is perfect so I give him 9/10 as human. I guarantee that your journey with him is going to be your best among all.",
-    },
-    {
-        title: "Ahnaf Ahmed Shoumik",
-        message:
-            "He has a very clear knowledge about android app development . Also has good python skills. I personally know him for years and found that he is very much committed to the work and very disciplined , punctual and trustworthy person . Works very hard in the projects. Has skills in c and c++ programming. Also took part in the programming contests . Highly recommend him for any kind of android app development.",
-    },
-]
 export default function Testimonial() {
     return (
         <>
             <h2>Testimonial</h2>
             <Carousel variant="dark" className={style.container}>
-                {contents.map((item, idx) => (
+                {config.testimonial.map((item, idx) => (
                     <Carousel.Item key={idx} className={style.item}>
                         <Carousel.Caption>
                             <h5>{item.title}</h5>
--- src/components/Pages/Resume/Languages.js
+++ src/components/Pages/Resume/Languages.js
@@ -20,15 +20,13 @@ import * as styles from "./Languages.module.css"
 export default function Languages() {
     return (
         <React.Fragment>
-            <Card body className={styles.container}>
-                <Row className={styles.row}>
+            <Card body>
+                <div className={styles.grid}>
                     <Icon wrapper={SiDjango} color="green" text="Django" />
                     <Icon wrapper={SiReact} color="blue" text="React.js" />
                     <Icon wrapper={SiReact} color="blue" text="Native" />
                     <Icon wrapper={SiGatsby} color="purple" text="Gatsby" />
-                </Row>
-                <hr />
-                <Row className={styles.row}>
+                    <hr />
                     <Icon wrapper={SiGit} color="grey" text="Git" />
                     <Icon wrapper={SiDocker} color="skyblue" text="Docker" />
                     <Icon wrapper={SiAmazonaws} color="black" text="AWS" />
@@ -38,9 +36,7 @@ export default function Languages() {
                         text="Postgresql"
                     />
                     <Icon wrapper={SiAmazons3} color="darkblue" text="AWS S3" />
-                </Row>
-                <hr />
-                <Row className={styles.row}>
+                    <hr />
                     <Icon
                         wrapper={SiScikitlearn}
                         color="orange"
@@ -49,7 +45,7 @@ export default function Languages() {
                     <Icon wrapper={SiTableau} color="purple" text="Tableau" />
                     <Icon wrapper={SiPowerbi} color="yellow" text="PowerBI" />
                     <Icon wrapper={SiPandas} color="orange" text="Pandas" />
-                </Row>
+                </div>
             </Card>
         </React.Fragment>
     )
