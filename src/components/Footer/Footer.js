8e9e82db024f6648deed13f2ce9ea95202d241c0
Tarek Ahmed <tarek5701@gmail.com>
2024-11-21 20:02:07 -0700

    Updates

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import * as styles from "./Footer.module.css"
-
-export default function Footer() {
-    return (
-        <footer className={styles.Footer}>Copyright © 2021 Tarek Ahmed</footer>
-    )
-}
--- src/components/Sidebar/Title.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import * as styles from "./Title.module.css"
-
-const Title = ({ title }) => {
-    return <h4 className={styles.line}>{title}</h4>
-}
-
-export default Title
